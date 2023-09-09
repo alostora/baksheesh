30b2a18e6d5847cd1c993447ea01e5a844e66612
alostora <dolsika5762@gmail.com>
2023-09-10 01:20:04 +0300

    prepare admin homw page widgets

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Constants\HasLookupType\UserAccountType;
+use App\Models\Company;
+use App\Models\SystemLookup;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('Admin/dashboard');
+
+        $client_type = SystemLookup::where('type', UserAccountType::LOOKUP_TYPE)
+            ->where('code',  UserAccountType::CLIENT['code'])
+            ->first();
+
+        $employee_type = SystemLookup::where('type', UserAccountType::LOOKUP_TYPE)
+            ->where('code',  UserAccountType::EMPLOYEE['code'])
+            ->first();
+
+        $data['count_active_clients'] = User::where('user_account_type_id', $client_type->id)
+            ->where('stopped_at', null)->count();
+
+        $data['count_active_employees'] = User::where('user_account_type_id', $employee_type->id)
+            ->where('stopped_at', null)->count();
+
+        $data['count_active_companies'] = Company::where('stopped_at', null)->count();
+
+        return view('Admin/dashboard', $data);
     }
 }
--- resources/views/Admin/content.blade.php
+++ resources/views/Admin/content.blade.php
@@ -5,71 +5,70 @@
   </h1>
 </section>
 <section class="content">
-    <div class="row">
-      <div class="col-lg-6 col-xs-6">
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h3>{{isset($parkedCarsCount) ? $parkedCarsCount : 0}}</h3>
-            <p>@lang('general.carCount')</p>
-          </div>
-          <div class="icon">
+  <div class="row">
+    <div class="col-lg-6 col-xs-6">
+      <div class="small-box bg-success">
+        <div class="inner">
+          <h3>{{$count_active_clients}}</h3>
+          <p>@lang('general.active_clients')</p>
+        </div>
+        <div class="icon">
           <i class="fa fa-car" aria-hidden="true"></i>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6 col-xs-6">
-        <div class="small-box bg-warning">
-          <div class="inner">
-            <h3>{{isset($saiesCount)?$saiesCount:0}}</h3>
-            <p>@lang('general.saiesCount')</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-users"></i>
-          </div>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-lg-4 col-xs-6">
-        <div class="small-box bg-primary">
-          <div class="inner">
-            <h3>
-              <span style="margin-left: 5%;">{{isset($totalTodayIncom) ? $totalTodayIncom:0}}</span>
-              
-            </h3>
-            <p>@lang('general.totalTodayIncom')</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-money"></i>
-          </div>
+    <div class="col-lg-6 col-xs-6">
+      <div class="small-box bg-warning">
+        <div class="inner">
+          <h3>{{$count_active_companies}}</h3>
+          <p>@lang('general.active_companies')</p>
         </div>
-      </div>
-      <div class="col-lg-4 col-xs-6">
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>
-                <span style="margin-left: 5%;">{{isset($totalMonthIncom)?$totalMonthIncom:0}}</span>
-                
-              </h3>
-              <p>@lang('general.totalMonthIncom')</p>
-            </div>
-            <div class="icon">
-            <i class="fa fa-dollar"></i>
-            </div>
-          </div>
-      </div>
-      <div class="col-lg-4 col-xs-6">
-        <div class="small-box bg-default">
-          <div class="inner">
-            <h3>
-              <span style="margin-left: 5%;">{{isset($totalYearIncom)?$totalYearIncom:0}}</span>
-            </h3>
-            <p>@lang('general.totalYearIncom')</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-dollar"></i>
-          </div>
+        <div class="icon">
+          <i class="fa fa-users"></i>
         </div>
       </div>
     </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-4 col-xs-6">
+      <div class="small-box bg-primary">
+        <div class="inner">
+
+          <h3>{{$count_active_employees}}</h3>
+          <p>@lang('general.active_employees')</p>
+
+        </div>
+        <div class="icon">
+          <i class="fa fa-money"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-xs-6">
+      <div class="small-box bg-danger">
+        <div class="inner">
+          <h3>
+            <span style="margin-left: 5%;">{{isset($totalMonthIncom)?$totalMonthIncom:0}}</span>
+
+          </h3>
+          <p>@lang('general.totalMonthIncom')</p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-dollar"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-xs-6">
+      <div class="small-box bg-default">
+        <div class="inner">
+          <h3>
+            <span style="margin-left: 5%;">{{isset($totalYearIncom)?$totalYearIncom:0}}</span>
+          </h3>
+          <p>@lang('general.totalYearIncom')</p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-dollar"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
