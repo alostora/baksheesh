e1fbfdbe3461be8e2836fa072bad3841d95ed0e6
ibrahim <hemmma100@gmail.com>
2024-02-01 12:43:33 +0200

    style-home- header

--- resources/views/Admin/content.blade.php
+++ resources/views/Admin/content.blade.php
@@ -101,7 +101,7 @@
     </div>
 
     <!-- charts -->
-    <div class="row">
+    <div class="charts row">
         <div class="col-md-6">
             <div class="box box-primary">
                 <div class="box-header with-border">
