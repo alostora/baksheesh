e3d751f1438b6ce8662a4ff6db61dda77ef6cf60
alostora <dolsika5762@gmail.com>
2024-02-02 05:07:07 +0200

    compelete dashboard

--- packages/Guest/src/Http/Requests/CompanyRatingRequest.php
+++ packages/Guest/src/Http/Requests/CompanyRatingRequest.php
@@ -6,36 +6,36 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CompanyRatingRequest extends FormRequest
 {
-     /**
-      * Determine if the user is authorized to make this request.
-      */
-     public function authorize(): bool
-     {
-          return true;
-     }
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-     /**
-      * Get the validation rules that apply to the request.
-      *
-      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-      */
-     public function rules(): array
-     {
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
 
 
-          return [
+        return [
 
-               "guest_key" => ["bail","required"],
+            "guest_key" => ["bail", "required"],
 
-               "rating_id" => ["required", "uuid", "string", "exists:system_lookups,id"],
+            "rating_id" => ["required", "uuid", "string", "exists:company_available_ratings,id"],
 
-               "rating_value" => ["bail", "required", "integer", "min:1", "max:2"],
+            "rating_value" => ["bail", "required", "integer", "min:1", "max:2"],
 
-               "payer_name" => ["bail", "nullable", "string", "max:255"],
+            "payer_name" => ["bail", "nullable", "string", "max:255"],
 
-               "payer_email" => ["bail", "nullable", "email", "unique:users,email", "max:255"],
+            "payer_email" => ["bail", "nullable", "email", "unique:users,email", "max:255"],
 
-               "payer_phone" => ["bail", "nullable", "string", "unique:users,phone", "max:255"],
-          ];
-     }
+            "payer_phone" => ["bail", "nullable", "string", "unique:users,phone", "max:255"],
+        ];
+    }
 }
